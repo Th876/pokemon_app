d8fba77c38fe28752431479c26e275acf795b004
TH876 <tayhiggins14@gmail.com>
2023-05-10 16:13:06 -0400

    I can view a list of all my pokemon in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,7 +1,8 @@
 const React = require('react');
+const pokemon = require('../models/pokemon');
 const myContainer = {
-    color: '#ffffff',
-    backgroundColor: '#000000',
+    color: '#e5989b',
+    backgroundColor: '#262626',
     width: '100vw',
     height: '100vh',
     marginTop: '-30px', 
@@ -14,16 +15,36 @@ const title = {
     paddingTop: '30px'
 }
 
-// const content {
-// 
-// }
+const content = {
+    color: '#ffe5d9',
+}
 
 
-function Index(props) {
+const Index = ({pokemon}) => {
+    // map through pokemon list and create a new array with capitalized names
+const capitalizePokemonName = pokemon.map((character) => ({
+    ...character,
+    name: character.name.charAt(0).toUpperCase() + character.name.slice(1),
+
+}));
+// In return, map through the new capitalized names array and display them, along with their image links
     return(
         <div style={myContainer}>
             <h1 style={title}>See All The Pokemon!</h1>
             
+            <ul style={content}>
+                
+                {capitalizePokemonName.map((character) => {
+                    return(
+                    <li key={character.name} style={{marginBottom: 
+                    '10px'}}>
+                        <b>Name: </b> {character.name}
+                        <br></br> 
+                        <b>Image:</b> {character.img}. 
+                    </li>
+                    );
+                })}
+            </ul>    
         </div>
     )
 }
