a6fcc6e8abd3383ec4baed7bb35fc78b62cbb0aa
TH876 <tayhiggins14@gmail.com>
2023-05-11 16:43:04 -0400

    Completed Part 2 with Pokedex

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 // require express
 const express = require('express');
 
-const pokemon = require('./models/pokemon.js');
+const Pokemon = require('./models/pokemon.js');
 
 // set express()to a variable
 const app = express();
 
 //set a variable of port to 3000
 const port = 3000;
+const mongoose = require('mongoose');
 
+// Add dotenv
+require('dotenv').config();
+
+// Mongoose info
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, 
+    useUnifiedTopology: true });
+    mongoose.connection.once('open', ()=> {
+        console.log('connected to mongo');
+    });
+    
 // Index : Show all the things!
 // New : An empty form for a new thing
 // Delete : Get rid of this particular thing!
@@ -17,10 +28,24 @@ const port = 3000;
 // Edit : A prefilled form to update a specific thing
 // Show : Show me this one thing!
 
+//MIDDLEWARE here. ALWAYS PLACE MIDDLEWARE BEFORE ROUTES
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+
+//near the top, around other app.use() calls 
+app.use(express.urlencoded({extended:true}));
+
+
 // View engines
 app.set('view engine', 'jsx');
 app.engine('jsx', require('jsx-view-engine').createEngine
 ());
+
+
+// Data
+// const Pokemon = require('./models/pokemon.js');
 
 // Routes here
 // include a get route /that will res.send('Welcome to the Pokemon App!');so that when you got to localhost:3000, you will see Welcome to the Pokemon App!
@@ -28,25 +53,51 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!');
 });
 
-app.get('/pokemon', (req, res) => {
-    res.render('../views/Index', { pokemon: pokemon });
+app.get('/pokemon', (req, res)=>{
+    Pokemon.find({}, (error, allPokemons)=>{
+        res.render('Index', {
+            pokemon: allPokemons
+        });
+    });
+});
+
+app.get('/pokemon/new', (req, res) => {
+    res.render('../views/New');
+});
+
+// app.get('/pokemon', (req, res) => {
+    // res.render('../views/Index', { pokemon: pokemon });
+// });
+
+app.post('/pokemon', (req, res)=>{
+    console.log('creating a pokemon');
+    console.log(req.body);
+    Pokemon.create(req.body, (error, createdPokemon)=>{
+        res.redirect('/pokemon');
+    });
 });
 
 // Setting up show route
-app.get('/pokemon/:id', (req, res) => {
+// app.get('/pokemon/:id', (req, res) => {
     // res.send(req.params.id);
-    res.render('../views/Show', {
-        pokemon: pokemon[req.params.id] //there 
-    }
-    )
-});
+    // res.render('../views/Show', {
+        // pokemon: pokemon[req.params.id] //there 
+    // }
+    // )
+// });
 
+app.get('/pokemon/:id', (req, res)=>{
+    Pokemon.findById(req.params.id, (err, foundPokemon)=>{
+        res.render('Show', {
+            pokemon : foundPokemon
+        });
+    });
+});
 
 
 
 // set your app to listen to the port and include a console.log(), so that you can tell when your server is running
 
 app.listen(port, () => {
-    console.log(`listening on port, $
-{port}`);
+    console.log(`listening on port, ${port}`);
 });
--- views/Show.jsx
+++ views/Show.jsx
@@ -1,7 +1,8 @@
 const React = require('react')
  const showContainer = {
-    backgroundImage: 'url(https://c4.wallpaperflare.com/wallpaper/725/791/792/pokemon-pikachu-wallpaper-preview.jpg)',
+    backgroundImage: 'url(https://cdn.wallpapersafari.com/98/7/Iu9XxP.jpg)',
     width: '100vw',
+    height: '100vh',
     backgroundSize :'cover',
     marginTop: '-10px', 
     marginLeft: '-20px',
@@ -10,7 +11,6 @@ const React = require('react')
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    height: '100vh',
  }
 
  const heading = {
@@ -21,10 +21,9 @@ const React = require('react')
  }
 
  const subheading = {
-    padding: '7px 30px',
     fontSize: '1.5rem',
-    color: '#000000',
-    backgroundColor: '#ffffff',
+    color: '#ffffff',
+
  }
 
  const buttonContainer = {
@@ -36,22 +35,23 @@ const React = require('react')
     textDecoration: 'none',
     color: '#CD565C',
     fontSize: '1.5rem',
-    backgroundColor: '#ffffff',
+    backgroundColor: '#f4f3ee',
     padding: '7px 15px',
     borderRadius: '8px',
+    boxShadow: '0px 5px 15px rgba(0, 0, 0, 0.35)',
+    fontWeight: 'bold',
 }
 
 const Show = ({ pokemon }) => {
   return (
   <div style={showContainer}>
     <h1 style={heading}>Gotta Catch 'Em All</h1>
-    <h2 style={subheading}>Name: {pokemon.name}</h2>
+    <h2 style={subheading}>Name: {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
     <b style={subheading}>Photo </b> <br>
     </br><img src={pokemon.img + ".jpg"} />  
     
     <div style={buttonContainer}>
             <a style={link} href="/pokemon">Back</a>
-
     </div>
 </div>
       );
